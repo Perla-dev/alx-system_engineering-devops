74538d0f6cbaa02fce9e1a0d191b76eb1bd57942
ZinoChan <devzinochan@gmail.com>
2023-08-27 10:07:03 +0100

    refactor

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,17 @@
 # client config
 
+include stdlib
+
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
+file_line { 'Delare identity file':
   ensure => present,
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile',
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
