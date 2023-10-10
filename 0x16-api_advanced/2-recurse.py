fdcd1c78bf690253004ecc0080f23fcbe2d73d58
ZinoChan <devzinochan@gmail.com>
2023-10-10 16:06:44 +0100

    add check for invalide subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,18 +3,16 @@
 import requests
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    if hot_list is None:
-        hot_list = []
-
+def recurse(subreddit, hot_list=[], after=None):
+    if subreddit is None or type(subreddit) is not str:
+        return None
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
 
-    params = {
-        'after': after
-    }
+    params = {"after": after}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         children = data["data"]["children"]
