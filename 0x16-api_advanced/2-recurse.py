2694d206745087978ae7e31637cc5c95e9075014
ZinoChan <devzinochan@gmail.com>
2023-10-10 15:42:15 +0100

    recursively getting hot topics from reddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import requests
+
+""" recursivly getting hot topics """
+
+
+def recurse(subreddit, hot_list=None, after=None):
+    if hot_list is None:
+        hot_list = []
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
+
+    response = requests.get(url, headers=headers, params=after)
+    print(response.status_code, response.json())
+    if response.status_code == 200:
+        data = response.json()
+        children = data["data"]["children"]
+
+        for child in children:
+            hot_list.append(child["data"]["title"])
+
+        after = data["data"]["after"]
+        if after:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    elif response.status_code == 404:
+        return None
+    else:
+        return None
